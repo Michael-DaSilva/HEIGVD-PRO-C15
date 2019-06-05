eed8ffe86d03226ec0d80504b9c22517cbca2944
Ethras <vladimirmbassi@gmail.com>
2019-06-05 18:13:16 +0200

    Bacterium page view

---
+++ inphinity-app/src/app/models/contig.ts
@@ -0,0 +1,6 @@
+export interface Contig {
+  id: number;
+  gene_contig: string[];
+  organism: number;
+  sequence_DNA: string;
+}
--- inphinity-app/src/app/views/bacterium-view/bacterium-view.component.ts
+++ inphinity-app/src/app/views/bacterium-view/bacterium-view.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {APIDatasService} from '../../services/apidatas.service';
+import {Bacterium} from '../../models/bacterium';
+import {flatMap} from 'rxjs/operators';
+import {Contig} from '../../models/contig';
+import {concat, Observable} from 'rxjs';
+import {WholeDNA} from '../../models/wholeDNA';
 
 @Component({
   selector: 'app-bacterium-view',
@@ -6,10 +12,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bacterium-view.component.scss']
 })
 export class BacteriumViewComponent implements OnInit {
+  bacterium = {organism_contig: [], organism_wholeDNA: []} as Bacterium;
+  personResponsible = '';
+  wholeContig = '';
+  wholeDNA = '';
 
-  constructor() { }
+  constructor(private api: APIDatasService) {
+
+  }
 
   ngOnInit() {
+    this.api.getDatas('/bacterium/5881/')
+      .pipe(flatMap((res: Bacterium) => {
+        this.bacterium = res;
+        console.log('bacterum', this.bacterium);
+
+        // DNA
+        concat(...this.bacterium.organism_wholeDNA.map(url => this.api.getDatasRawUrl(url) as Observable<WholeDNA>)).subscribe(
+          contig => this.wholeDNA += contig.sequence_DNA
+        );
+
+        // Contig
+        concat(...this.bacterium.organism_contig.map(url => this.api.getDatasRawUrl(url) as Observable<Contig>)).subscribe(
+          contig => this.wholeContig += contig.sequence_DNA
+        );
+        return this.api.getDatas('/personresp/' + res.person_responsible + '/');
+      })).subscribe(person => this.personResponsible = person.name);
+
   }
 
 }
